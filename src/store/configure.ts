8be3c5b4a283422238f81c73a0d7c6dbb04ad8e2
i.myronova <i.myronova@andersenlab.com>
2022-03-11 15:46:10 +0200

    remove cypress

--- src/store/configure.ts
+++ src/store/configure.ts
@@ -2,9 +2,6 @@ import { configureStore, isPlain } from '@reduxjs/toolkit';
 
 import rootReducer from 'store/rootReducer';
 
-// serializableCheck ругается объекты созданные через конструктор https://github.com/reduxjs/redux-toolkit/blob/next/src/isPlainObject.ts
-// const isInstanceOfModel = (value: any) => value?.__proto__.constructor.name === 'Model';
-
 const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
@@ -16,12 +13,5 @@ const store = configureStore({
       },
     }),
 });
-
-// expose store when run in Cypress
-// @ts-ignore
-if (window.Cypress && __DEV__) {
-  // @ts-ignore
-  window.store = store;
-}
 
 export default store;
